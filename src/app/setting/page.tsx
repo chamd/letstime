31ce3ebef5b595c636762255cbb8c058f22d246d
chamd <chana051226@gmail.com>
2025-09-27 21:23:54 +0900

    add copy data, add set data

--- src/app/setting/page.tsx
+++ src/app/setting/page.tsx
@@ -27,12 +27,43 @@ const BlockButton = ({ onClick, children }: {
 };
 
 const Setting = () => {
+	const handleCopyData = async (name: string) => {
+		const text = localStorage.getItem(name) || "";
+		try {
+      await navigator.clipboard.writeText(text); 
+    } catch (err) {
+      alert("Failed to copy text");
+			console.error(err);
+    }
+	}
+
+	const handleInputData = (name: string) => {
+		const text = prompt("input JSON", "") || "";
+		localStorage.setItem(name, text);
+	}
+
 	const handleClearData = (name: string) => {
 		localStorage.setItem(name, "{}");
 	}
 
 	return (
 		<div className="p-4 flex flex-col gap-3">
+			<Block>
+				고정 스케쥴 데이터 복사
+				<BlockButton onClick={() => handleCopyData("scheduleData")}>복사</BlockButton>
+			</Block>
+			<Block>
+				하루 스케쥴 데이터 복사
+				<BlockButton onClick={() => handleCopyData("subScheduleData")}>복사</BlockButton>
+			</Block>
+			<Block>
+				고정 스케쥴 데이터 입력
+				<BlockButton onClick={() => handleInputData("scheduleData")}>입력</BlockButton>
+			</Block>
+			<Block>
+				하루 스케쥴 데이터 입력
+				<BlockButton onClick={() => handleInputData("subScheduleData")}>입력</BlockButton>
+			</Block>
 			<Block>
 				고정 스케쥴 데이터 삭제
 				<BlockButton onClick={() => handleClearData("scheduleData")}>삭제</BlockButton>
