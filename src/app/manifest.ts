576e5249024449c5bc156b407018b8dff580f8d2
chamd <chana051226@gmail.com>
2025-09-28 17:23:22 +0900

    update logo

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -9,38 +9,14 @@ export default function manifest(): MetadataRoute.Manifest {
     background_color: "#f8fafc",
     icons: [
       {
-        src: "/icon/1024.png",
-        sizes: "1024x1024",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
         src: "/icon/512.png",
         sizes: "512x512",
         type: "image/png",
         purpose: "any",
       },
       {
-        src: "/icon/192.png",
-        sizes: "192x192",
-        type: "image/png",
-        purpose: "any",
-      },
-      {
-        src: "/icon/1024.png",
-        sizes: "1024x1024",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
         src: "/icon/512.png",
         sizes: "512x512",
-        type: "image/png",
-        purpose: "maskable",
-      },
-      {
-        src: "/icon/192.png",
-        sizes: "192x192",
         type: "image/png",
         purpose: "maskable",
       },
